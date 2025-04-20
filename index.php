795401ca229251276b71a28ed646ade3b1163823
hlhol <126687748+hlhol@users.noreply.github.com>
2025-04-20 21:02:29 +0300

    add all views in my mind

--- index.php
+++ index.php
@@ -1,7 +1,5 @@
 <?php
 
-/* 
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHP.php to edit this template
- */
-
+$view = new stdClass();
+$view->pageTitle = 'Homepage';
+require_once('Views/index.phtml');
