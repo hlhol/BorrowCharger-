b1715f0fd53b403aeec0e68565a197cf9b502979
hlhol <126687748+hlhol@users.noreply.github.com>
2025-04-22 14:09:40 +0300

    add login without js valdition and add erd img

--- login.php
+++ login.php
@@ -2,4 +2,23 @@
 session_start();
 $view = new stdClass();
 $view->pageTitle = 'Login';
+require_once('Models/login.php');
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $identifier = trim($_POST['identifier']);
+    $password = $_POST['pass'];
+
+    $login = new Login();
+    $user = $login->auth($identifier, $password);
+
+    if ($user) {
+        $_SESSION['login'] = true;  
+        $_SESSION['user_role'] = $user['role']; 
+        header('Location: dashboard.php'); 
+        exit;
+    } else {
+        $view->error = "Invalid login credentials.";
+    }
+}
+
 require_once('Views/login.phtml');
