959c90c3e15a9fd715ab3881639a73a960063172
hlhol <126687748+hlhol@users.noreply.github.com>
2025-05-08 23:05:45 +0300

    make map localy not using cdn

--- Public/js/map/map.js
+++ Public/js/map/map.js
@@ -1,24 +1,24 @@
 const map = L.map('map').setView([53.483710, -2.270110], 13);
 
 const availableIcon = L.icon({
-    iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png',
-    shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png',
+    iconUrl: 'lib/leaflet/images/marker-icon.png',
+    shadowUrl: 'lib/leaflet/images/marker-icon.png',
     iconSize: [25, 41],
     iconAnchor: [12, 41],
     popupAnchor: [1, -34]
 });
 
 const unavailableIcon = L.icon({
-    iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-red.png',
-    shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png',
+    iconUrl: 'lib/leaflet/images/marker-icon-red.png',
+    shadowUrl: 'lib/leaflet/images/marker-icon-red.png',
     iconSize: [25, 41],
     iconAnchor: [12, 41],
     popupAnchor: [1, -34]
 });
 
 const userIcon = L.icon({
-    iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-green.png',
-    shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png',
+    iconUrl: 'lib/leaflet/images/marker-icon-green.png',
+    shadowUrl: 'lib/leaflet/images/marker-icon-green.png',
     iconSize: [25, 41],
     iconAnchor: [12, 41],
     popupAnchor: [1, -34]
