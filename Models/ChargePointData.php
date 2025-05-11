87e5901809661e75c4ce5cf28ebaf59d0d8ad42f
hlhol <126687748+hlhol@users.noreply.github.com>
2025-05-11 13:40:19 +0300

    don with map

--- Models/ChargePointData.php
+++ Models/ChargePointData.php
@@ -167,24 +167,6 @@ public function create(int $ownerId, array $data): bool {
        return $results;
    }
 
-   public function fetchFilter(array $data){
-       $stmt = $this->conn->prepare(
-         "SELECT * FROM charge_points WHERE "
-                 
-       );
-       
-       $result  = [];
-       while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-           $results[] = new cpModel($row);
-       }
-       return $results;
-   }
-
-
-
-
-
-
    public function countAllChargePoints(): int {
     $stmt = $this->conn->prepare(
         "SELECT COUNT(*) FROM charge_points"  // This counts all charge points, not just available ones
